83d6244a5fc7cf665f69a21ed704856b5ccb0791
huyluongme <huyluongme.cs@gmail.com>
2024-08-04 14:33:44 +0700

    tcp completed

---
+++ tcp_client.cpp
@@ -0,0 +1,58 @@
+/*
+    Author: huyluongme
+    TCP Client
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+int main() {
+    int client_fd;
+    const char* hello = "Hello from client";
+    char buffer[1024] = {0};
+
+    if((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("Socket creation error");
+        return -1;
+    }
+
+    printf("Client socket is created successful\n");
+
+    sockaddr_in server_addr;
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8080);
+
+    if(connect(client_fd, (sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        printf("Connection failed");
+        return -1;
+    } 
+
+    printf("Connect to Server successful\n");
+
+    send(client_fd, hello, strlen(hello), 0);
+
+    read(client_fd, buffer, 1024 - 1);
+
+    printf("%s\n", buffer);
+
+    while (true)
+    {
+        char mess[100];
+        scanf("%[^\n]%*c", mess);
+        send(client_fd, mess, strlen(mess), 0);
+        int valread = read(client_fd, mess, 100);
+        mess[valread] = '\0';
+        if(strcmp(mess, "bye") == 0) break;
+        printf("%s\n", mess);
+    }
+    
+
+    close(client_fd);
+    return 0;
+}
--- tcp_server.cpp
+++ tcp_server.cpp
@@ -1,55 +1,86 @@
-#include <iostream>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+/*
+    Author: huyluongmme
+    TCP Server
+*/
+#include <stdio.h>
 #include <cstring>
-
-using namespace std;
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
 
 int main() {
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 0), newSocket;
-    int opt = 1;
+    int server_fd, new_socket;
     ssize_t valread;
-    char buffer[1024] = { 0 };
-    char* hello = "Hello from server";
+    sockaddr_in server_addr;
+    int opt = 1;
+    socklen_t server_addr_len = sizeof(server_addr);
+    char buffer[1024] = {0};
+    const char* hello = "Hello from server";
+    const char* reply = "Server recieved mess";
 
-    if(serverSocket < 0) {
-        perror("create socket failed");
+    // create server socket file descriptor
+    if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    if(setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        perror("socketopt");
+    printf("Server socket is created successful\n");
+
+    // reuse address and port
+    if(setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
         exit(EXIT_FAILURE);
     }
 
-    sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
-    serverAddress.sin_port = htons(8080);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8080);
 
-    if(bind(serverSocket, (sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
+    if(bind(server_fd, (sockaddr*)&server_addr, server_addr_len) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
 
-    if(listen(serverSocket, 3) < 0) {
+    printf("bind successful\n");
+
+    if(listen(server_fd, 3) < 0) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
 
-    if((newSocket = accept(serverSocket, (sockaddr*)&serverAddress, (socklen_t *)(sizeof(serverAddress)))) < 0) {
+    printf("Listening from clients\n");
+
+    if((new_socket = accept(server_fd, (sockaddr*)&server_addr, &server_addr_len)) < 0) {
         perror("accept");
         exit(EXIT_FAILURE);
     }
 
-    while(true) {
-        valread = read(newSocket, buffer, 1024 - 1);
-        printf("%s\n", buffer);
-        send(newSocket, hello, strlen(hello), 0);
-    }
+    printf("Accepted\n");
+    
 
-    close(newSocket);
-    close(serverSocket);
+    read(new_socket, buffer, 1024 - 1);
+
+    printf("%s\n", buffer);
+    send(new_socket, hello, strlen(hello), 0);
+
+    while (true)
+    {   
+        char mess[100];
+        valread = read(new_socket, mess, 100);
+        mess[valread] = '\0';
+
+        if(strcmp(mess, "bye") == 0) {
+            send(new_socket, mess, strlen(mess), 0);
+            break;
+        }
+
+        printf("%s\n", mess);
+        send(new_socket, reply, strlen(reply), 0);
+    }
+    
+
+    close(new_socket);
+    close(server_fd);
     return 0;
 }
